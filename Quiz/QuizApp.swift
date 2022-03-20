8f8be27e05c1731b84d94fa9901454b14b794173
QuizApps <benibara3758@gmail.com>
2022-03-20 19:10:14 +0900

    Initial Commit

---
+++ Quiz/QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizApp.swift
+//  Quiz
+//
+//  Created by jaemin park on 2022/03/20.
+//
+
+import SwiftUI
+
+@main
+struct QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
